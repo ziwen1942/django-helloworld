1716ffdd80d85d2fd05c28c3153f60424c8e3ecf
root <root@ziwen-test.novalocal>
2019-02-13 08:14:29 +0000

    5th commit

---
+++ supervisor.sh
@@ -0,0 +1,11 @@
+mkdir -p /opt/django/log/
+touch /opt/django/log/django.txt
+
+touch /etc/supervisor/conf.d/django.conf
+
+echo -e "[program:django] \ncommand=python /root/django-helloworld/manage.py runserver 0.0.0.0:8000\nautorestart=true\nstartsecs=5\nstartretries=10\nredirect_stderr=true\nstdout_logfile=/opt/django/log/django.txt\nstderr_logfile=/opt/django/log/django.txt" >> /etc/supervisor/conf.d/django.txt
+
+supervisorctl update
+supervisord -c /etc/supervisor/supervisord.conf
+
+curl http://192.168.180.117:8000/index/
