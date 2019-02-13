f929ce8ed2e0807c12b96bc190988d410f6564b6
root <root@ziwen-test.novalocal>
2019-02-13 02:14:34 +0000

    secound commit

--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
 apt-get update
-apt-get install python
-apt-get install python-pip
-apt-get install git
-pip install Django==1.11.18
+apt-get -y install python
+apt-get -y install python-pip
+apt-get -y install git
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Django==1.11.18
+pip install supervisor -i https://pypi.tuna.tsinghua.edu.cn/simple
 
+rm -rf django-helloworld/
 git clone https://github.com/ziwen1942/django-helloworld.git
-python django-helloworld/manage.py runserver 0.0.0.0:8000 
+# python django-helloworld/manage.py runserver 0.0.0.0:8000 
 
